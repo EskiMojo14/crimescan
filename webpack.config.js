3668b2f217be577b6d4df5045f4f7028bddf20a5
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-12 12:30:31 +0100

    Added eslint and prettier.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,52 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
-  devtool: "inline-source-map",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "index_bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(js)$/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index_bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                enforce: 'pre',
+                use: [
+                    {
+                        options: {
+                            eslintPath: require.resolve('eslint'),
+                        },
+                        loader: require.resolve('eslint-loader'),
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader', 'eslint-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(js)$/,
+                use: ['babel-loader', 'eslint-loader'],
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+        }),
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  mode: "development",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "public/index.html",
-    }),
-  ],
 };
 
 // typescript: https://webpack.js.org/guides/typescript/
