bb99973e8d646062d593f31c3aee96f583267e37
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-12 23:34:34 +0100

    Moved lat and long fields onto same line.

--- src/components/input/DrawerSettings.tsx
+++ src/components/input/DrawerSettings.tsx
@@ -94,61 +94,63 @@ export const DrawerSettings = (props: DrawerSettingsProps) => {
           <Typography use="overline" tag="div" className="subheader">
             Location
           </Typography>
-          <div className="field">
-            <TextField
-              outlined
-              label="Latitude"
-              icon={iconObject(
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  version="1.1"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M12 4C15 4 17.5 5.6 18.9 8H5.1C6.5 5.6 9 4 12 4M12 20C9 20 6.5 18.4 5.1 16H18.9C17.5 18.4 15 20 12 20M4.3 14C4.1 13.4 4 12.7 4 12S4.1 10.6 4.3 10H19.8C20 10.6 20.1 11.3 20.1 12S20 13.4 19.8 14H4.3Z" />
-                </svg>
-              )}
-              name="lat"
-              required
-              pattern="^(-?\d+(\.\d+)?)$"
-              value={lat}
-              onChange={handleChange}
-              helpText={{
-                persistent: false,
-                validationMsg: true,
-                children: lat.length > 0 && !latLngRegex.test(lat) ? "Invalid format" : "This field is required",
-              }}
-            />
-          </div>
-          <div className="field">
-            <TextField
-              outlined
-              label="Longitude"
-              icon={iconObject(
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  version="1.1"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2A10 10 0 1 0 22 12A10.03 10.03 0 0 0 12 2M9.4 19.6A8.05 8.05 0 0 1 9.4 4.4A16.45 16.45 0 0 0 7.5 12A16.45 16.45 0 0 0 9.4 19.6M12 20A13.81 13.81 0 0 1 9.5 12A13.81 13.81 0 0 1 12 4A13.81 13.81 0 0 1 14.5 12A13.81 13.81 0 0 1 12 20M14.6 19.6A16.15 16.15 0 0 0 14.6 4.4A8.03 8.03 0 0 1 20 12A7.9 7.9 0 0 1 14.6 19.6Z" />
-                </svg>
-              )}
-              name="lng"
-              required
-              pattern="^(-?\d+(\.\d+)?)$"
-              value={lng}
-              onChange={handleChange}
-              helpText={{
-                persistent: false,
-                validationMsg: true,
-                children: lng.length > 0 && !latLngRegex.test(lng) ? "Invalid format" : "This field is required",
-              }}
-            />
+          <div className="double-field">
+            <div className="field">
+              <TextField
+                outlined
+                label="Latitude"
+                icon={iconObject(
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    version="1.1"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M12 4C15 4 17.5 5.6 18.9 8H5.1C6.5 5.6 9 4 12 4M12 20C9 20 6.5 18.4 5.1 16H18.9C17.5 18.4 15 20 12 20M4.3 14C4.1 13.4 4 12.7 4 12S4.1 10.6 4.3 10H19.8C20 10.6 20.1 11.3 20.1 12S20 13.4 19.8 14H4.3Z" />
+                  </svg>
+                )}
+                name="lat"
+                required
+                pattern="^(-?\d+(\.\d+)?)$"
+                value={lat}
+                onChange={handleChange}
+                helpText={{
+                  persistent: false,
+                  validationMsg: true,
+                  children: lat.length > 0 && !latLngRegex.test(lat) ? "Invalid format" : "This field is required",
+                }}
+              />
+            </div>
+            <div className="field">
+              <TextField
+                outlined
+                label="Longitude"
+                icon={iconObject(
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    version="1.1"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2A10 10 0 1 0 22 12A10.03 10.03 0 0 0 12 2M9.4 19.6A8.05 8.05 0 0 1 9.4 4.4A16.45 16.45 0 0 0 7.5 12A16.45 16.45 0 0 0 9.4 19.6M12 20A13.81 13.81 0 0 1 9.5 12A13.81 13.81 0 0 1 12 4A13.81 13.81 0 0 1 14.5 12A13.81 13.81 0 0 1 12 20M14.6 19.6A16.15 16.15 0 0 0 14.6 4.4A8.03 8.03 0 0 1 20 12A7.9 7.9 0 0 1 14.6 19.6Z" />
+                  </svg>
+                )}
+                name="lng"
+                required
+                pattern="^(-?\d+(\.\d+)?)$"
+                value={lng}
+                onChange={handleChange}
+                helpText={{
+                  persistent: false,
+                  validationMsg: true,
+                  children: lng.length > 0 && !latLngRegex.test(lng) ? "Invalid format" : "This field is required",
+                }}
+              />
+            </div>
           </div>
           <div className="guide-chips">
             {validLocation ? <Chip icon="location_on" label="Query" className="query-chip non-interactive" /> : null}
