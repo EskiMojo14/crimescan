ba4ada57eed8b62b75979891c801c9166a669a48
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-12 11:39:21 +0100

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Basic setup from https://javascript.plainenglish.io/a-guide-to-creating-a-react-app-without-create-react-app-5337c5ac2ea0
