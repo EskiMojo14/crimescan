335e44755bc436501bbe4ad3ac6b6ce5b324e425
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-12 15:09:39 +0100

    Added basic theming and theme toggle.

--- src/components/main/DrawerSettings.tsx
+++ src/components/main/DrawerSettings.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Drawer, DrawerHeader, DrawerContent } from "@rmwc/drawer";
+import { IconButton } from "@rmwc/icon-button";
 import { Logo } from "../util/Logo";
 import "./DrawerSettings.scss";
 
 type DrawerSettingsProps = Record<string, never>;
 
 export const DrawerSettings = (props: DrawerSettingsProps) => {
+  const [theme, setTheme] = useState("dark");
+  const changeTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+    document.documentElement.className = newTheme;
+  };
   return (
     <Drawer dismissible open className="drawer-settings">
       <DrawerHeader>
         <Logo className="drawer-logo" />
-        <span className="logo-text">CrimeScan</span>
+        <div className="logo-text">CrimeScan</div>
+        <IconButton icon={theme === "dark" ? "dark_mode" : "light_mode"} onClick={changeTheme} />
       </DrawerHeader>
       <DrawerContent></DrawerContent>
     </Drawer>
