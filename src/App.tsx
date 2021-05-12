c69a00a0501b061509f318f97acc95ccb51589fd
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-12 13:43:23 +0100

    Added example usage of the police API.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import "./App.scss";
 import { Logo } from "./components/util/Logo";
 
 function App() {
+  const getData = () => {
+    const month = "2020-02";
+    const lat = "52.629729";
+    const lng = "-1.131592";
+    fetch(`https://data.police.uk/api/crimes-at-location?date=${month}&lat=${lat}&lng=${lng}`)
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return <Logo rotate />;
 }
 export default App;
