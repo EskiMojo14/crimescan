2a40662f821a2bc8de480cd376b62202d866f4ac
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-12 20:36:53 +0100

    Added types for crime entry, and example data.

---
+++ src/util/constants.tsx
@@ -0,0 +1,106 @@
+import { CrimeEntry } from "./types";
+
+export const exampleData: CrimeEntry[] = [
+  {
+    category: "anti-social-behaviour",
+    location_type: "Force",
+    location: {
+      latitude: "51.378370",
+      street: {
+        id: 539044,
+        name: "On or near Shopping Area",
+      },
+      longitude: "-2.359207",
+    },
+    context: "",
+    outcome_status: null,
+    persistent_id: "",
+    id: 82806818,
+    location_subtype: "",
+    month: "2020-04",
+  },
+  {
+    category: "public-order",
+    location_type: "Force",
+    location: {
+      latitude: "51.378370",
+      street: {
+        id: 539044,
+        name: "On or near Shopping Area",
+      },
+      longitude: "-2.359207",
+    },
+    context: "",
+    outcome_status: {
+      category: "Court result unavailable",
+      date: "2020-10",
+    },
+    persistent_id: "6a4e4a4f2565ffcc71d6962ec79ca9a0237fad47668bf92612daa9737c20acb6",
+    id: 82814102,
+    location_subtype: "",
+    month: "2020-04",
+  },
+  {
+    category: "public-order",
+    location_type: "Force",
+    location: {
+      latitude: "51.378370",
+      street: {
+        id: 539044,
+        name: "On or near Shopping Area",
+      },
+      longitude: "-2.359207",
+    },
+    context: "",
+    outcome_status: {
+      category: "Court result unavailable",
+      date: "2020-10",
+    },
+    persistent_id: "53a2d3d174f73dcbe4dce9d44e2689bd485fcb4b5bebcf68f3ba33e6a9158a01",
+    id: 82812901,
+    location_subtype: "",
+    month: "2020-04",
+  },
+  {
+    category: "violent-crime",
+    location_type: "Force",
+    location: {
+      latitude: "51.378370",
+      street: {
+        id: 539044,
+        name: "On or near Shopping Area",
+      },
+      longitude: "-2.359207",
+    },
+    context: "",
+    outcome_status: {
+      category: "Unable to prosecute suspect",
+      date: "2020-04",
+    },
+    persistent_id: "726434b6a68c7555ef983d0af31bb81cb66801bc83b1d1fedfed5be584657f5a",
+    id: 82809488,
+    location_subtype: "",
+    month: "2020-04",
+  },
+  {
+    category: "violent-crime",
+    location_type: "Force",
+    location: {
+      latitude: "51.378370",
+      street: {
+        id: 539044,
+        name: "On or near Shopping Area",
+      },
+      longitude: "-2.359207",
+    },
+    context: "",
+    outcome_status: {
+      category: "Court result unavailable",
+      date: "2020-11",
+    },
+    persistent_id: "0866a8e6ead67e40e1f5d3a2dd3295f0a7fd0ab79aaa8e527be26e4bb7f6eb59",
+    id: 82811909,
+    location_subtype: "",
+    month: "2020-04",
+  },
+];
--- src/util/types.ts
+++ src/util/types.ts
@@ -3,3 +3,27 @@ import React from "react";
 /** Alias for standard HTML props. */
 
 export type HTMLProps = React.HTMLAttributes<HTMLElement>;
+
+/** Crime entry format, based on https://data.police.uk/docs/method/crimes-at-location/ */
+
+export type CrimeEntry = {
+  category: string;
+  location_type: string;
+  location: {
+    latitude: string;
+    street: {
+      id: number;
+      name: string;
+    };
+    longitude: string;
+  };
+  context: string;
+  outcome_status: null | {
+    category: string;
+    date: string;
+  };
+  persistent_id: string;
+  id: number;
+  location_subtype: string;
+  month: string;
+};
