a871c16d18af4848f64a5be4dba7f1ba0485dd18
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-12 20:22:03 +0100

    Implemented snackbars for error display.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import store from "./app/store";
+import { Provider } from "react-redux";
+import { queue } from "./app/snackbarQueue";
+import { SnackbarQueue } from "@rmwc/snackbar";
 import { DrawerAppContent } from "@rmwc/drawer";
 import { DrawerSettings } from "./components/input/DrawerSettings";
 import "./App.scss";
 import "normalize.css";
-import { Provider } from "react-redux";
-import store from "./app/store";
 
 function App() {
   const getData = (month: string, lat: string, lng: string) => {
@@ -17,6 +19,8 @@ function App() {
       })
       .catch((error) => {
         console.log(error);
+        queue.notify({ title: "Failed to get crime data: " + error });
+        setLoading(false);
       });
   };
   const [loading, setLoading] = useState(false);
@@ -24,6 +28,7 @@ function App() {
     <Provider store={store}>
       <DrawerSettings loading={loading} getData={getData} />
       <DrawerAppContent></DrawerAppContent>
+      <SnackbarQueue messages={queue.messages} />
     </Provider>
   );
 }
