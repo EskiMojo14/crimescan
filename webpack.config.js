e3ff0173ae7f749e9c8e44e36cdf8eec6a959f85
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-12 12:53:20 +0100

    Ensure webpack copies over assets from /public.

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,63 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.tsx',
-    devtool: 'inline-source-map',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index_bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                enforce: 'pre',
-                use: [
-                    {
-                        options: {
-                            eslintPath: require.resolve('eslint'),
-                        },
-                        loader: require.resolve('eslint-loader'),
-                    },
-                ],
-                exclude: /node_modules/,
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        enforce: "pre",
+        use: [
+          {
+            options: {
+              eslintPath: require.resolve("eslint"),
             },
-            {
-                test: /\.tsx?$/,
-                use: ['ts-loader', 'eslint-loader'],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(js)$/,
-                use: ['babel-loader', 'eslint-loader'],
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
+            loader: require.resolve("eslint-loader"),
+          },
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    mode: 'development',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-        }),
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: ["ts-loader", "eslint-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js)$/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          context: "./public/",
+          from: "*.{ico,png,json}",
+          to: ".",
+        },
+      ],
+    }),
+  ],
 };
 
 // typescript: https://webpack.js.org/guides/typescript/
