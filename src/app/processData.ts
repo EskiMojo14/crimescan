0b0e4119920edde7de289fe1680dfa329824aa21
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-13 12:27:39 +0100

    Create function to format category names with provided values.

--- src/app/processData.ts
+++ src/app/processData.ts
@@ -1,12 +1,26 @@
 import type { CrimeEntry, MonthQuery, ProcessedData, YearQuery } from "../util/types";
-import { uniqueArray, alphabeticalSort } from "../util/functions";
+import { uniqueArray, alphabeticalSort, hasKey } from "../util/functions";
+import store from "./store";
+
+export const formatCategory = (category: string) => {
+  const state = store.getState();
+  const { formattedCategories } = state.display;
+  if (hasKey(formattedCategories, category)) {
+    return formattedCategories[category];
+  }
+  return category;
+};
 
 export const processMonthData = (data: CrimeEntry[], query: MonthQuery): ProcessedData => {
   const firstEntry = data[0];
   const location = firstEntry ? `${firstEntry.location.latitude}, ${firstEntry.location.longitude}` : "";
   const queryLocation = `${query.lat},${query.lng}`;
 
-  const allCategories = alphabeticalSort(uniqueArray(data.map((entry) => entry.category)));
+  const formattedEntries: CrimeEntry[] = data.map((entry) => {
+    return { ...entry, category: formatCategory(entry.category) };
+  });
+
+  const allCategories = alphabeticalSort(uniqueArray(formattedEntries.map((entry) => entry.category)));
 
   const count = data.length;
   return {
@@ -23,11 +37,17 @@ export const processYearData = (data: CrimeEntry[][], query: YearQuery): Process
   const location = firstEntry ? `${firstEntry.location.latitude}, ${firstEntry.location.longitude}` : "";
   const queryLocation = `${query.lat},${query.lng}`;
 
-  const count = data.map((arr) => arr.length);
+  const formattedEntries: CrimeEntry[][] = data.map((entries) =>
+    entries.map((entry) => {
+      return { ...entry, category: formatCategory(entry.category) };
+    })
+  );
 
   const allCategories = alphabeticalSort(
-    uniqueArray(data.map((entries) => entries.map((entry) => entry.category)).flat(1))
+    uniqueArray(formattedEntries.map((entries) => entries.map((entry) => entry.category)).flat(1))
   );
+
+  const count = data.map((arr) => arr.length);
 
   return { type: "year", location: location, queryLocation: queryLocation, allCategories: allCategories, count: count };
 };
