e851c2217cf95ac1b2746b21ad301ac27eddcf5a
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-14 16:56:18 +0100

    Added tests for SegmentedButtonSegment and rewrote tests to not use direct node access.

--- src/components/util/SegmentedButton.unit.test.tsx
+++ src/components/util/SegmentedButton.unit.test.tsx
@@ -1,19 +1,45 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import { SegmentedButton, SegmentedButtonSegment } from "./SegmentedButton";
 
 describe("<SegmentedButton />", () => {
   it("generates element with necessary class", async () => {
-    const { container } = render(<SegmentedButton />);
-    expect(container.firstChild).toHaveClass("segmented-button");
+    render(<SegmentedButton data-testid="button" />);
+    const button = screen.getByTestId("button");
+    expect(button).toHaveClass("segmented-button");
   });
   it("adds toggle class if prop is added", async () => {
-    const { container } = render(<SegmentedButton toggle />);
-    expect(container.firstChild).toHaveClass("segmented-button--toggle");
+    render(<SegmentedButton toggle data-testid="button" />);
+    const button = screen.getByTestId("button");
+    expect(button).toHaveClass("segmented-button--toggle");
   });
   it("generates element with additional classes if specified", async () => {
-    const { container } = render(<SegmentedButton className="test-class" />);
-    expect(container.firstChild).toHaveClass("test-class");
+    render(<SegmentedButton className="test-class" data-testid="button" />);
+    const button = screen.getByTestId("button");
+    expect(button).toHaveClass("test-class");
+  });
+});
+
+describe("<SegmentedButtonSegment />", () => {
+  it("generates element with necessary class", async () => {
+    render(<SegmentedButtonSegment />);
+    const button = screen.getByRole("button");
+    expect(button).toHaveClass("segmented-button__segment");
+  });
+  it("adds selected class if prop is added", async () => {
+    render(<SegmentedButtonSegment selected />);
+    const button = screen.getByRole("button");
+    expect(button).toHaveClass("segmented-button__segment--selected");
+  });
+  it("adds `only-icon` modifier if only an icon is provided", async () => {
+    render(<SegmentedButtonSegment icon="home" />);
+    const button = screen.getByRole("button");
+    expect(button).toHaveClass("segmented-button__segment--only-icon");
+  });
+  it("generates element with additional classes if specified", async () => {
+    render(<SegmentedButtonSegment className="test-class" />);
+    const button = screen.getByRole("button");
+    expect(button).toHaveClass("test-class");
   });
 });
